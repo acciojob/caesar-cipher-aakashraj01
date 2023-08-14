5660422aa8e569060beb9cea5a431566f3f9d9f6
Aakash Raj <79682170+aakashraj01@users.noreply.github.com>
2023-08-14 20:36:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function caesarCipher(message, shift) {
-  // add your code here
+  const A_CODE = "A".charCodeAt(0);
+  const Z_CODE = "Z".charCodeAt(0);
+  const a_CODE = "a".charCodeAt(0);
+  const z_CODE = "z".charCodeAt(0);
+
+  let encodedMessage = "";
+
+  for (let i = 0; i < message.length; i++) {
+    const charCode = message.charCodeAt(i);
+
+    if (charCode >= A_CODE && charCode <= Z_CODE) {
+      const newCharCode = ((charCode - A_CODE + shift) % 26) + A_CODE;
+      encodedMessage += String.fromCharCode(newCharCode);
+    } else if (charCode >= a_CODE && charCode <= z_CODE) {
+      const newCharCode = ((charCode - a_CODE + shift) % 26) + a_CODE;
+      encodedMessage += String.fromCharCode(newCharCode);
+    } else {
+      encodedMessage += message[i];
+    }
+  }
+
+  return encodedMessage;
 }
 
-// Do not change this line
 window.caesarCipher = caesarCipher;
